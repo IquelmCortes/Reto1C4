cad34c72ffd51d93bc23a6c3a8dcd8b9d3c2f801
Miguel Cortés <92167462+IquelmCortes@users.noreply.github.com>
2021-10-27 09:19:10 -0500

    Update CategoryController.java

--- src/main/java/com/example/demo/controllers/CategoryController.java
+++ src/main/java/com/example/demo/controllers/CategoryController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/Category")
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST})
 public class CategoryController {
 
     @Autowired
