2d47eeb50b2e8a852d034f0665527fc493acb80a
Miguel Cortés <92167462+IquelmCortes@users.noreply.github.com>
2021-11-01 12:29:54 -0500

    Update CabinController.java

--- src/main/java/com/example/demo/controllers/CabinController.java
+++ src/main/java/com/example/demo/controllers/CabinController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/Cabin")
-@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST})
+@CrossOrigin
 public class CabinController {
 
     @Autowired
